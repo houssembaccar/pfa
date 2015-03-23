86816055581dcec78e934a65eccf30b35e17c109
Houssem <houssembaccar@gmail.com>
2015-03-23 14:52:34 +0100

    lire tout les nom des conceptes par leurs ordre d'apparition dans le fichier.owl les mettre dans un arraylist conceptList) et les afficher sur la console

--- pfa/src/main/Input.java
+++ pfa/src/main/Input.java
@@ -2,7 +2,7 @@ package main;
 
 import java.io.*;
 import java.util.*;
-
+import com.pfa.beans.*;
 public class Input{
 	
 	private Scanner scanner;
@@ -16,12 +16,23 @@ public class Input{
 		}
 	}
 	public void readFile (){
+		int indexOfFirstQuote;
+		int indexOfSecondQuote;
+		ArrayList<Concept> conceptList= new ArrayList<Concept>();
 		
 		while(scanner.hasNext()){
 			String currentLine=scanner.nextLine();
 			if (currentLine.contains("<rdfs:Class rdf:ID"))
-			System.out.printf("%s \n",currentLine);
-			
+			{
+				indexOfFirstQuote=currentLine.indexOf("\"");
+				indexOfSecondQuote=currentLine.lastIndexOf("\"");
+				
+				conceptList.add(new Concept(currentLine.substring(indexOfFirstQuote+1, indexOfSecondQuote)));
+			}
+		
+		}
+		for(int i=0;i<conceptList.size();i++) {
+			System.out.println(conceptList.get(i).getName());
 		}
 	}
 	public void closeFile(){
