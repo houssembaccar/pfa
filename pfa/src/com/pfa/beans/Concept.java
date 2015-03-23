fbba0334a1e2cc8dcc4e77301e2860db41fc18b1
Houssem <houssembaccar@gmail.com>
2015-03-23 19:18:54 +0100

    j'ai ajouter la classe Concept au projet

---
+++ pfa/src/com/pfa/beans/Concept.java
@@ -0,0 +1,19 @@
+package com.pfa.beans;
+
+public class Concept {
+	
+	private String name;
+	
+	public Concept(String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
